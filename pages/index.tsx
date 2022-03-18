599fddd7ac512db724991b5f59c9057ab1a3a891
Kristian Ivancevic <kristian@get-local.app>
2022-03-18 17:52:17 +0100

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    
     <html className={styles.container} >
         <Head>
           <title>Borealis</title>
@@ -17,7 +16,7 @@ const Home: NextPage = () => {
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=optional" />
         </Head>
         <main className={styles.main}>
           <div className={styles.header}>
